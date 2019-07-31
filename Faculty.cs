fd588744cd280b244bdfd4cdb97f724aa578a446
Momarul Islam <36568905+momerul@users.noreply.github.com>
2019-07-31 13:23:49 +0600

    Add files via upload

---
+++ Faculty.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace BanglaMvc.Models
+{
+    public class FacultyDetails
+    {
+        public int SerialNo { get; set; }
+        public string Name { get; set; }
+        public string Designation { get; set; }
+        public string Photograph { get; set; }
+        public string Academic_Background { get; set; }
+    }
+
+    public class StudentDetails
+    {
+        public int ID { get; set; }
+        public int Roll { get; set; }
+        public string StudentName { get; set; }
+        public int Mid_1 { get; set; }
+        public int Mid_2 { get; set; }
+        public int Final { get; set; }
+        public int Total { get; set; }
+    }
+
+    public class Faculty
+    {
+        public int ID { get; set; }
+        public string Department { get; set; }
+
+        public static string Connection
+        {
+            get
+            {
+                return ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString.ToString();
+            }
+        }
+
+        public List<FacultyDetails> GetAllFaculty()
+        {
+            SqlParameter[] parameter = new SqlParameter[1];
+            parameter[0] = Faculty.AddParameter("@ID", ID, System.Data.SqlDbType.Int, 10);
+            List<FacultyDetails> dt = Faculty.SPExecute("sp_getFaculty", parameter);
+            return dt;
+        }
+        public List<StudentDetails> GetAllStudents()
+        {
+            SqlParameter[] parameter = new SqlParameter[1];
+            parameter[0] = Faculty.AddParameter("@ID", ID, System.Data.SqlDbType.Int, 10);
+            List<StudentDetails> dt = Faculty.SPExecute1("GetStudentInformationByID", parameter);
+            return dt;
+        }
+
+        public List<StudentDetails> AllStudents()
+        {
+            List<StudentDetails> dt = Faculty.SPExecute2("GetAllStudents");
+            return dt;
+        }
+
+        public static SqlParameter AddParameter(string parameter, object value, SqlDbType type, int size)
+        {
+            SqlParameter param = new SqlParameter();
+            param.ParameterName = parameter;
+            param.Value = value.ToString();
+            param.SqlDbType = type;
+            param.Size = size;
+            param.Direction = ParameterDirection.Input;
+            return param;
+        }
+
+        public static List<FacultyDetails> SPExecute(string ProcedureName, SqlParameter[] para)
+        {
+            SqlConnection con = new SqlConnection(Connection);
+            SqlCommand scmd = new SqlCommand(ProcedureName, con);
+            scmd.Parameters.AddRange(para);
+            scmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(scmd);
+            DataTable dt = new DataTable();
+
+            try
+            {
+                dataAdapter.Fill(dt);
+            }
+            catch (Exception)
+            {
+
+            }
+            finally
+            {
+                dataAdapter.Dispose();
+                scmd.Dispose();
+                scmd.Parameters.Clear();
+                con.Dispose();
+            }
+
+            List<FacultyDetails> list = new List<FacultyDetails>();
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                FacultyDetails userinfo = new FacultyDetails();
+                userinfo.Name = dt.Rows[i]["Name"].ToString();
+                userinfo.Designation = dt.Rows[i]["Designation"].ToString();
+                userinfo.Photograph = dt.Rows[i]["Photograph"].ToString();
+                userinfo.Academic_Background = dt.Rows[i]["Academic_Background"].ToString();
+                list.Add(userinfo);
+            }
+
+            return list;
+        }
+
+        public static List<StudentDetails> SPExecute1(string ProcedureName, SqlParameter[] para)
+        {
+            SqlConnection con = new SqlConnection(Connection);
+            SqlCommand scmd = new SqlCommand(ProcedureName, con);
+            scmd.Parameters.AddRange(para);
+            scmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(scmd);
+            DataTable dt = new DataTable();
+
+            try
+            {
+                dataAdapter.Fill(dt);
+            }
+            catch (Exception)
+            {
+
+            }
+            finally
+            {
+                dataAdapter.Dispose();
+                scmd.Dispose();
+                scmd.Parameters.Clear();
+                con.Dispose();
+            }
+
+            List<StudentDetails> list = new List<StudentDetails>();
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                StudentDetails userinfo = new StudentDetails();
+                userinfo.Roll = Convert.ToInt32(dt.Rows[i]["Roll"]);
+                userinfo.StudentName = dt.Rows[i]["StudentName"].ToString();
+                userinfo.Mid_1 = Convert.ToInt32(dt.Rows[i]["Mid_1"]);
+                userinfo.Mid_2 = Convert.ToInt32(dt.Rows[i]["Mid_2"]);
+                userinfo.Final = Convert.ToInt32(dt.Rows[i]["Final"]);
+                userinfo.Total = Convert.ToInt32(dt.Rows[i]["Total"]);
+                list.Add(userinfo);
+            }
+
+            return list;
+        }
+
+        public static List<StudentDetails> SPExecute2(string ProcedureName)
+        {
+            SqlConnection con = new SqlConnection(Connection);
+            SqlCommand scmd = new SqlCommand(ProcedureName, con);
+            scmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(scmd);
+            DataTable dt = new DataTable();
+
+            try
+            {
+                dataAdapter.Fill(dt);
+            }
+            catch (Exception)
+            {
+
+            }
+            finally
+            {
+                dataAdapter.Dispose();
+                scmd.Dispose();
+                scmd.Parameters.Clear();
+                con.Dispose();
+            }
+
+            List<StudentDetails> list = new List<StudentDetails>();
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                StudentDetails userinfo = new StudentDetails();
+                userinfo.Roll = Convert.ToInt32(dt.Rows[i]["Roll"]);
+                userinfo.StudentName = dt.Rows[i]["StudentName"].ToString();
+                userinfo.Mid_1 = Convert.ToInt32(dt.Rows[i]["Mid_1"]);
+                userinfo.Mid_2 = Convert.ToInt32(dt.Rows[i]["Mid_2"]);
+                userinfo.Final = Convert.ToInt32(dt.Rows[i]["Final"]);
+                userinfo.Total = Convert.ToInt32(dt.Rows[i]["Total"]);
+                list.Add(userinfo);
+            }
+
+            return list;
+        }
+    }
+}
---
+++ HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BanglaMvc.Models;
+using System.Data;
+
+namespace BanglaMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public ActionResult Details(int Id)
+        {
+            Faculty faculty = new Faculty()
+            {
+                ID = Id
+            };
+            List<FacultyDetails> dt = faculty.GetAllFaculty();
+            return View(dt);
+        }
+
+        public ActionResult AllStudents()
+        {
+            Faculty faculty = new Faculty();
+            List<StudentDetails> details= faculty.AllStudents();
+            return View(details);
+        }
+
+        YYYEntities student = new YYYEntities();
+
+        public ActionResult AllStudent(string Search)
+        {
+            return View(student.StudentDetails.Where(x => x.StudentName.Contains(Search) || Search == null).ToList());
+        }
+
+
+        public ActionResult StudentDetails(int Id)
+        {
+            Faculty faculty = new Faculty()
+            {
+                ID = Id
+            };
+            List<StudentDetails> dt = faculty.GetAllStudents();
+            return View(dt);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact Page.";
+
+            return View();
+        }       
+    }
+}
